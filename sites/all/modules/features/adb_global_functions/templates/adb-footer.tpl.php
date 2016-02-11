b18b2a58a54dcbe8a8b69f930a7f94260f72d0ec
Subhojit Paul <subhojitpaul21@gmail.com>
2016-02-11 12:29:42 +0530

    ADB-61: Site footer

---
+++ sites/all/modules/features/adb_global_functions/templates/adb-footer.tpl.php
@@ -0,0 +1,10 @@
+<div class="footer-wrapper">
+  <div class="logo-wrapper"><?php print $logo_markup; ?></div>
+  <div class="site-info-wrapper">
+    <div class="name-slider-wrapper">
+      <div class="site-name"><?php print $site_name; ?></div><?php print $slider_markup; ?>
+    </div>
+    <div class="copyright">&copy; <?php print $copyright_text; ?></div>
+  </div>
+  <div class="social-links-wrapper"><?php print $social_links; ?></div>
+</div>
