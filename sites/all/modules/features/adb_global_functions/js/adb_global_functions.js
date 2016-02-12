2a990717b7ed1ee4a0d04e247a614532f637d1d7
Subhojit Paul <subhojitpaul21@gmail.com>
2016-02-12 15:51:55 +0530

    ADB-54: Bug fix menu customizations are not working

--- sites/all/modules/features/adb_global_functions/js/adb_global_functions.js
+++ sites/all/modules/features/adb_global_functions/js/adb_global_functions.js
@@ -1,7 +1,7 @@
 (function ($) {
   Drupal.behaviors.adbGlobalFunctions = {
     attach: function(context, settings) {
-      var mainMenuLinks = $('#main-menu-links').children();
+      var mainMenuLinks = $('.primary-menu').children();
 
       closeMenu();
       closeOtherMenus();
@@ -19,11 +19,11 @@
 
       // Make sure only one menu is opened at a time.
       function closeOtherMenus() {
-        $('.menu-attach-block-drop-link').click(function (event) {
+        $('.menu-attach-block-drop-link').click(function() {
           var self = $(this);
 
           // Close all menus except itself.
-          if (!self.hasClass('dropped')) {
+          if (self.hasClass('dropped')) {
             mainMenuLinks.each(function() {
               if (!$(this).is(self.parent())) {
                 $(this).find('.menu-attach-block-wrapper').slideUp('fast');
