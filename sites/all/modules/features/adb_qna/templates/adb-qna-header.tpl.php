14762e31fab8cdfc7361253e2eb78c6585db9be2
Subhojit Paul <subhojitpaul21@gmail.com>
2016-02-18 12:17:26 +0530

    ADB-31: Changed logic for rendering header in QnA

--- sites/all/modules/features/adb_qna/templates/adb-qna-header.tpl.php
+++ sites/all/modules/features/adb_qna/templates/adb-qna-header.tpl.php
@@ -5,14 +5,16 @@
  *
  * Available variables:
  * - $type_name: Content type name.
- * - $current_term: Taxonomy term object whose landing page you are viewing.
+ * - $current_term_name: Taxonomy term name whose landing page you are viewing.
+ * - $current_term_description: Taxonomy term description whose landing page
+ *   you are viewing.
  * - $siblings: Sibling taxonomy term object of $current_term.
  */
 ?>
 <div class="qna-header-wrapper">
   <div class="type-name"><?php print $type_name; ?></div>
   <div class="term-name-cards-wrapper">
-    <div class="term-name"><?php print $current_term->name; ?></div>
+    <div class="term-name"><?php print $current_term_name; ?></div>
     <div class="cards-wrapper">
       <div class="cards-toggler"><?php print t('Change cards'); ?></div>
       <div class="cards-toggle-wrapper">
@@ -22,5 +24,5 @@
       </div>
     </div>
   </div>
-  <div class="type-description"><?php print $current_term->description; ?></div>
+  <div class="type-description"><?php print $current_term_description; ?></div>
 </div>
